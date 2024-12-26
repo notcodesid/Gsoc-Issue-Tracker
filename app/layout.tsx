028bff73cdff8b495c5ee3aa998fd91c9072f00f
siddharth <notcodesid@gmail.com>
2024-12-26 13:03:22 -0500

    add open graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,32 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'GSOC Issue Tracker',
   description: 'Track and discover organizations participating in Google Summer of Code',
+
+
+  // Open Graph metadata
+  openGraph: {
+    title: 'GSOC Issue Tracker',
+    description: 'Track and discover organizations participating in Google Summer of Code',  
+    url: "https://todos-tau-woad.vercel.app/",
+    siteName: "GSOC Issue Tracker",
+    images: [
+      {
+        url:"/open-graph.png", // image link
+        width: 1200,
+        height: 500,
+        alt: "Title with their description",
+      },
+    ],
+    locale: "en_US",
+    type: "article",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: 'GSOC Issue Tracker',
+    description: 'Track and discover organizations participating in Google Summer of Code',  
+    images: ["/open-graph.png"],
+  }
 };
 
 export default function RootLayout({
